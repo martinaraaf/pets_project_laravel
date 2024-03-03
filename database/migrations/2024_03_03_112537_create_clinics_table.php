cb11d4b7521308fbf065fa7e1790fd45c3939660
Hagar <hagermoh21@gmail.com>
2024-03-03 16:31:20 +0200

    feed DB table 'clinics', with the data from file json after putting it in the seeder.

---
+++ database/migrations/2024_03_03_112537_create_clinics_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('clinics', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('place');
+            $table->string('place_area');
+            $table->string('city');
+            $table->string('rate')->nullable();
+            $table->text('opening_hours')->nullable();
+            $table->char('phone_number', 15)->nullable();
+            $table->string('url')->nullable();
+            $table->text('images')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('clinics');
+    }
+};
