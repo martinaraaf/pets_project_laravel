2e0f4b094b687b0ffc8a5c69f708e02f29d49108
Hagar <hagermoh21@gmail.com>
2024-03-06 21:19:32 +0200

    added resource for the clinics, and unwrapped it.

--- app/Http/Controllers/API/ClinicController.php
+++ app/Http/Controllers/API/ClinicController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\API;
 use App\Http\Controllers\Controller;
 use App\Models\Clinic;
 use Illuminate\Http\Request;
+use App\Http\Resources\ClinicResource;
 
 class ClinicController extends Controller
 {
@@ -13,7 +14,9 @@ class ClinicController extends Controller
     {
         $clinics = Clinic::all();
         // return response()->json($clinics);
-        return $clinics;
+        // return new ClinicResource($clinics);
+        // $x = ClinicResource::collection($clinics);
+        return ClinicResource::collection($clinics);
     }
 
     public function search(Request $request)
@@ -39,12 +42,15 @@ class ClinicController extends Controller
         $clinics = $query->get();
 
         // return response()->json($clinics);
-        return $clinics;
+        // return $clinics;
+        return ClinicResource::collection($clinics);
     }
 
     public function show(Clinic $id)
     {
-        // return response()->json($clinics);
-        return $id;
+        // return response()->json($id);
+        // return $id;
+        return new ClinicResource($id);
+
     }
 }
